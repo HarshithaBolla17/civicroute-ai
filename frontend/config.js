4965f31cfffbb53f175c53172f1deae7d71582fd
HarshithaBolla17 <harshithabolla872@gmail.com>
2026-07-24 16:03:15 +0530

    Delete frontend/config.js

--- frontend/config.js
+++
@@ -1,3 +0,0 @@
-// After you deploy the backend to Render, replace the URL below with
-// your real backend URL, e.g. "https://civicroute-backend.onrender.com"
-   const API_BASE_URL = "https://civicroute-ai.onrender.com";
